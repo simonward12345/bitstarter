3212194570cc1cbdac8f07f01542e54bda809eb4
Ubuntu <ubuntu@ip-172-31-1-57.us-west-2.compute.internal>
2013-07-21 18:46:19 +0000

    updated web.js to load from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
-
+var buff = new Buffer(fs.readFile('index.html'));
+var content = buff.toString;
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var f = fs.readFile('index.html');
+  response.send(content);
+
 });
 
 var port = process.env.PORT || 5000;
